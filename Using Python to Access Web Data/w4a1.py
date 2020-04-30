5574ef6bfe895dfb8fffb2adc99dcca07dafa43c
Prakalp Tiwari <48635172+PrakalpTiwari137@users.noreply.github.com>
2020-04-30 21:51:27 +0530

    Week 4 Assignment 1

--- Using Python to Access Web Data/w4a1.py
+++ Using Python to Access Web Data/w4a1.py
@@ -1,1 +1,16 @@
+import urllib.request, urllib.parse,urllib.error
+from bs4 import BeautifulSoup
 
+# url = 'http://py4e-data.dr-chuck.net/comments_481549.html'
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html,'html.parser')
+
+val = 0
+
+# find all elements having span tag 
+lst = soup.find_all('span')
+for line in lst:
+    val = val + int(line.get_text())    # get the contents of the tag
+print(val)
